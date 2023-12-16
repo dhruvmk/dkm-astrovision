50f7ff9e73f9cd1016458562039805d7c27d84e1
Dhruv Kapu <65883953+dhruvmk@users.noreply.github.com>
2023-12-16 17:24:54 -0500

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -4,21 +4,22 @@ import zipfile
 from huggingface_hub import hf_hub_download
 
 # Zip files of segments to extract
-zipfiles = {
-    "clusters/train/dawn_vesta/00000000.zip": "00000000",
-    "clusters/train/dawn_vesta/00000001.zip": "00000001",
-    "clusters/train/dawn_vesta/00000002.zip": "00000002",
-    "clusters/train/dawn_vesta/00000003.zip": "00000003"
-}
+zipfiles = {}
+for i in range(4):
+    number = str(i).zfill(8)
+    zipfiles["clusters/train/dawn_vesta/" + number + ".zip"] = number
+
+print(zipfiles)
 
 for file in zipfiles:
+    '''
     hf_hub_download(
         repo_id="travisdriver/astrovision-data",
         filename=file,
         repo_type="dataset",
         local_dir="data",
         local_dir_use_symlinks=False
-    )
+    )'''
 
     with zipfile.ZipFile("data/" + file, "r") as zip_ref:
-        zip_ref.extractall("data/" + zipfiles[file])
+        zip_ref.extractall("data/")

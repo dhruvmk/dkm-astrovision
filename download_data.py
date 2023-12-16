2ef60b0e2beac2611162e8f31bbf798300358507
Dhruv Kapu <65883953+dhruvmk@users.noreply.github.com>
2023-12-16 16:00:45 -0500

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -5,12 +5,10 @@ from huggingface_hub import hf_hub_download
 
 # Zip files of segments to extract
 zipfiles = {
-    "segments/cas_epim/opus.zip": "cas_epim",
-    "segments/cas_mimas/sbmt.zip": "cas_mimas",
-    "segments/dawn_ceres/2015293_c6_orbit125.zip": "dawn_ceres",
-    "segments/dawn_vesta/2011205_rc3.zip": "dawn_vesta",
-    "segments/haya_itokawa/20050909_20051119.zip": "haya_itokawa",
-    "segments/rosetta_lutetia/sbmt.zip": "rosetta_lutetia"
+    "clusters/train/dawn_vesta/00000000.zip": "00000000",
+    "clusters/train/dawn_vesta/00000001.zip": "00000001",
+    "clusters/train/dawn_vesta/00000002.zip": "00000002",
+    "clusters/train/dawn_vesta/00000003.zip": "00000003"
 }
 
 for file in zipfiles:
